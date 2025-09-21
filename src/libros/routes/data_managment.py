15bd4dea159da8844d38ecce1923d5429a40c98a
SerGioAMM <sergiomurcia01@gmail.com>
2025-09-21 15:21:11 -0600

    🎉 ✨ Importar libros, casi funcional

--- src/libros/routes/data_managment.py
+++ src/libros/routes/data_managment.py
@@ -1,30 +1,96 @@
-from flask import Blueprint, session, redirect, request,render_template,url_for
+from flask import Blueprint, session, redirect, request,render_template,url_for, jsonify, send_file
 from datetime import datetime
 import math
 from src.database.db_sqlite import conexion_BD
+import pandas as panda
 
 bp_data_managment = Blueprint('data_managment',__name__, template_folder="../templates")
 
 # ----------------------------------------------------- IMPORTAR LIBROS ----------------------------------------------------- #
+from time import sleep
+import threading
+from io import BytesIO
+progreso = {"valor":0, "total":0, "actual":0, "error": "", "contador_errores":0}
+errores = []
+
 
 @bp_data_managment.route("/importar_libros", methods=["GET", "POST"])
 def importar_libros():
     if "usuario" not in session:
         return redirect("/") #Solo se puede acceder con session iniciada
+    global progreso 
+    progreso["valor"] = 0
+    progreso["total"] = 0
+    progreso["actual"] = 0
+    progreso["contador_errores"] = 0
+    datos = ''
+    if request.method == "POST":
+        if "archivo_excel" not in request.files:
+            return render_template("importar_libros.html", alerta="No se subió archivo")
+        archivo = request.files["archivo_excel"]
+        if archivo.filename == "":
+            return render_template("importar_libros.html", alerta="Nombre de archivo vacío")
+        try:
+            datos = panda.read_excel(archivo)
+            total = len(datos)
+            # Lanzar la importación en un hilo aparte
+            hilo = threading.Thread(target=importar, args=(datos,total))
+            hilo.start()
+        except Exception as e:
+            print(f"Error: {e}")
+            alerta = "Error al leer el archivo."
+            return render_template("importar_libros.html", alerta=alerta) 
 
-    conexion = conexion_BD()
-    query = conexion.cursor()
-    try:
-        pass
-    except Exception as e:
-        print(f"Error: {e}")
-        alerta = "Error al ingresar libro."
-        return render_template("importar_libros.html", alerta=alerta) 
-    finally:
-        query.close()
-        conexion.close()
+    return render_template("importar_libros.html", datos=datos)
 
-    return render_template("importar_libros.html")
+
+def importar(datos,total):
+    global errores
+    errores = []
+    progreso["error"] = ("DATOS INCORRECTOS EN FILA: ")
+    for index,row in datos.iterrows():
+        autor = row['AUTOR']
+        titulo = row['TITULO DEL LIBRO']
+        lugar = row['LUGAR DE PUBLICACIÓN']
+        editorial = row['EDITORIAL']
+        anio = row['AÑO']
+        num_paginas = row['No. DE PAGINAS']
+        ISBN = row['ISBN']
+        tomo = row['TOMO']
+        seccion = row['CÓDIGO']
+        notacion = row['NOTACIÓN INTERNA']
+        num_copias = row['No. DE COPIAS']
+        
+        if(panda.isna(titulo)):
+            progreso["error"] = progreso["error"] + (f"{index+2}, ")
+            errores.append(row.to_dict())
+            progreso["contador_errores"] += 1
+        progreso["total"] = total
+        progreso["actual"] = index + 1
+        progreso["valor"] = round((((index + 1) / total) * 100),0)
+        #sleep(0.1)
+
+@bp_data_managment.route("/progreso")
+def get_progreso():
+    return jsonify(progreso)
+
+@bp_data_managment.route("/descargar_errores")
+def descargar_errores():
+    if not errores:
+        return "No hay errores para descargar", 400
+    
+    output = BytesIO()
+    excel = panda.DataFrame(errores)
+    excel.to_excel(output, index=False)
+    output.seek(0) 
+
+    return send_file(
+        output,
+        as_attachment=True,
+        download_name="errores.xlsx",
+        mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+
 
 
 # ----------------------------------------------------- EXPORTAR LIBROS ----------------------------------------------------- #
--- src/static/js/progreso.js
+++ src/static/js/progreso.js
@@ -2,12 +2,15 @@ const barra = document.getElementById("barra");
 const actual = document.getElementById("actual");
 const total = document.getElementById("total");
 const form = document.getElementById("form-excel");
-const actualizaciones = document.getElementById("Actualizaciones");
+const logs = document.getElementById("logs");
 let intervalo = null;
+const progressbar = document.getElementById("progressbar");
+let i = 0;
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
-
+    progressbar.classList.remove("d-none");
+    logs.innerHTML = "";
     const formData = new FormData(form);
 
     fetch(form.action, {
@@ -17,7 +20,7 @@ form.addEventListener("submit", function(e) {
     .then(response => response.text())
     .then(data => {
         agregarLog("Importación iniciada", "info");
-        
+        let descargar_errores = false;
         if (intervalo) clearInterval(intervalo);
         intervalo = setInterval(() => {
             fetch("/progreso")
@@ -25,19 +28,23 @@ form.addEventListener("submit", function(e) {
                 .then(data => {
                     barra.style.width = data.valor + "%";
                     barra.innerText = data.valor + "%";
-                    actual.textContent = data.actual;
+                    actual.textContent = data.actual - data.contador_errores;
                     total.textContent = data.total;
-                    console.log("PROGRESO: "+data.valor + "%");
-                agregarLog(("Progreso"+data.valor + "%"), "success");
+                    if(data.error){
+                        descargar_errores = true;
+                    }
                     
-                    if (data.valor >= 100) {
+                    if ((data.valor >= 100)) {
+                        if(descargar_errores){
+                            agregarLog(("Error: "+data.error), "danger");
+                            agregarLog("Descargar errores","descargar_errores")
+                        }
                         clearInterval(intervalo);
                         intervalo = null;
                         agregarLog("Importación finalizada", "secondary");
-
                     }
             });
-        }, 1000);
+        }, 600);
     })
     .catch(error => {
         console.error("Error al enviar el formulario:", error);
@@ -46,15 +53,26 @@ form.addEventListener("submit", function(e) {
 
 function agregarLog(mensaje, tipo = "info") {
     // tipo puede ser: info, success, warning, danger
-    const logs = document.getElementById("logs");
 
     const card = document.createElement("div");
-    card.className = `card my-2 border-${tipo}`;
+    card.className = `card my-1 text-left border-${tipo}`;
 
     const body = document.createElement("div");
-    body.className = "card-body p-2 text-left";
+    body.className = "card-body p-2 text-start";
 
-    body.innerHTML = `<span class="badge bg-${tipo} text-uppercase">${tipo}</span> ${mensaje}`;
+    body.innerHTML = `<span class="badge text-${tipo} ">
+                        <i class="bi bi-circle-fill"></i>
+                        <span class="vr mx-2" ></span>
+                        </span> ${mensaje}`;
+
+    if(tipo == "descargar_errores"){
+        card.className = `card my-1 text-left border-warning`;
+        body.innerHTML = `<span class="badge text-warning ">
+                        <i class="bi bi-circle-fill"></i>
+                        <span class="vr mx-2" ></span>
+                        </span><a class="btn btn-warning" href="/descargar_errores">${mensaje}</a>`;
+    }
+    
     
     card.appendChild(body);
     logs.appendChild(card);
